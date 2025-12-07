85b4415870862d7b97bdf760201b82050fdef9b3
Binyamin <binyaminf22@gmail.com>
2025-12-07 22:40:37 +0100

    deploy

--- quartz.config.ts
+++ quartz.config.ts
@@ -100,7 +100,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Plugin.CustomOgImages(),
+      Plugin.CustomOgImages({ colorScheme: "darkMode" }),
     ],
   },
 }
